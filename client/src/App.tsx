6dbc23a7c07a0492106ac01d8f5d1e0b1687483e
raied-b-alharbi <raied.b.alharbi@gmail.com>
2026-02-20 00:47:13 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,13 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import MonthlyReports from "./pages/MonthlyReports";
-import NotificationCenter from "./pages/NotificationCenter";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path={"/login"} component={Login} />
-      <Route path={"/"} component={Home} />
-      <Route path={"/admin"} component={AdminDashboard} />
-      <Route path={"/reports"} component={MonthlyReports} />
-      <Route path={"/notifications"} component={NotificationCenter} />
-      <Route path={"/404"} component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <TooltipProvider>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold">تم رفع الموقع بنجاح!</h1>
+      </div>
       <Toaster />
-      <Router />
     </TooltipProvider>
   );
 }
