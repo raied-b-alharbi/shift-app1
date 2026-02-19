22accf48c4b32911a8d5014d2ef8d988b1e8bde6
raied-b-alharbi <raied.b.alharbi@gmail.com>
2026-02-19 23:27:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { manusRuntime } from "vite-plugin-manus-runtime";
 
 export default defineConfig({
   plugins: [
     react(),
-    manusRuntime(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
-  },
-  server: {
-    middlewareMode: true,
   },
   build: {
     target: "ES2020",
